91722fd0f5e883bef8a2a5b23d219f3af7e04e85
Jonathan Hinkel <asder0815@gmail.com>
2021-06-07 21:18:22 +0200

    created project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h1>Machine Learning Prototype</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">GitHub Repostory</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <MLMain/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MLMain from './components/MLMain';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    MLMain,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/MLMain.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello Machine Learning</h1>
+</template>
+
+<script>
+  export default {
+    name: 'MLMain',
+
+    data: () => ({
+    }),
+  }
+</script>
